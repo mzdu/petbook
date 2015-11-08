ac57168dcdd766a6ce3c60470afdace6d6d587a9
Hongying <tsingpg@hotmail.com>
2015-11-08 15:28:00 -0500

    fixed click on my own posts goes to the my profile page

--- www/app/directives.js
+++ www/app/directives.js
@@ -62,9 +62,11 @@
         console.log('vm.cardType = ', vm.cardType);
 
         if(vm.cardType === 'myPosts'){
+            //console.log('enter myposts');
             vm.showProfileAvatar = true;
-            vm.showPostAvatar = false;
+            vm.showPostAvatar = true;
         } else {
+            //console.log('enter moments');
             vm.showProfileAvatar = false;
             vm.showPostAvatar = true;
         }
@@ -129,11 +131,19 @@
         };
 
         function checkFriendInfo(post){
-           //console.log(post._Owner._id);
-           console.log(post._Owner);
-            $state.go('app.friendinfo',{
-                userID: post._Owner._id
-            });
+           
+           /*console.log(post._Owner);
+           console.log(post._Owner._id);
+           console.log(user);*/
+           //for moments page, _Owner is a user, 
+           //for my posts page, _Owner is a user id
+           if(user._id == post._Owner || user._id == post._Owner._id){
+                $state.go('app.profile');
+           }else{
+                $state.go('app.friendinfo',{
+                    userID: post._Owner._id
+                });
+            }
          };
 
         
