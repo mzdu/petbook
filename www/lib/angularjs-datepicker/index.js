f0d0d44b84f840ebc7c5e65e47a3ea7293b9f9b5
david <beetz12@gmail.com>
2015-12-20 08:29:00 -0500

    removed bower packages

--- www/lib/angularjs-datepicker/index.js
+++
@@ -1,8 +0,0 @@
-/*global module, require*/
-(function setUp(module, require) {
-  'use strict';
-
-  require('./src/js/angular-datepicker');
-
-  module.exports = '720kb.datepicker';
-}(module, require));
